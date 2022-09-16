930345c66dff6905dd4a9d109b3d7cd7a4b162d4
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 22:40:57 +0100

    The final verse can be sung now.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -3,6 +3,8 @@ require 'minitest/autorun'
 class BottlesSongTest < Minitest::Test
   def verse(number = 99)
     case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around no more bottles of beer on the wall."
     when 2
@@ -30,5 +32,10 @@ class BottlesSongTest < Minitest::Test
   def test_verse_98
     expected_verse = "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around no more bottles of beer on the wall."
     assert_equal(expected_verse, verse(1))
+  end
+
+  def test_last_verse
+    expected_verse = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    assert_equal(expected_verse, verse(0))
   end
 end
