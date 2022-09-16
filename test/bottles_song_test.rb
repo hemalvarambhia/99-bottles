599c49cfb18563916a7a1d501c03ca6958be4a5b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 22:35:43 +0100

    Removed the duplication.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -7,9 +7,7 @@ class BottlesSongTest < Minitest::Test
       "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around 1 bottle of beer on the wall."
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around no more bottles of beer on the wall."
-    when 99
-      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottles of beer on the wall."
-    when 98
+    else
       "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottles of beer on the wall."
     end
   end
