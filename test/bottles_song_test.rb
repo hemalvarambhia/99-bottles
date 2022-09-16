4bfea2942a4c54ebd3c7637ee647be86be0aad96
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 22:43:17 +0100

    Made similar code even more similar.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -6,9 +6,9 @@ class BottlesSongTest < Minitest::Test
     when 0
       "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
     when 1
-      "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around no more bottles of beer on the wall."
+      "#{number} bottle of beer on the wall, #{number} bottle of beer. Take it down and pass it around no more bottles of beer on the wall."
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around 1 bottle of beer on the wall."
+      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottle of beer on the wall."
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottles of beer on the wall."
     end
