ce9920c59505af8c2acd818074a07a1b65ca5915
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 23:25:12 +0100

    Corrected a test, extracted a function to make similar code even more similar.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -4,23 +4,32 @@ class BottlesSongTest < Minitest::Test
   def verse(number = 99)
     case number
     when 0
-      "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+      "No more bottles of beer on the wall, no more bottles of beer. #{action(number)}, 99 bottles of beer on the wall."
     when 1
-      "#{number} bottle of beer on the wall, #{number} bottle of beer. Take it down and pass it around, no more bottles of beer on the wall."
+      "#{number} bottle of beer on the wall, #{number} bottle of beer. #{action(number)}, no more bottles of beer on the wall."
     when 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottle of beer on the wall."
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around #{number - 1} bottles of beer on the wall."
+      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down and pass it around, #{number - 1} bottles of beer on the wall."
+    end
+  end
+
+  def action(number)
+    case number
+    when 0
+      "Go to the store and buy some more"
+    when 1
+      "Take it down and pass it around"
     end
   end
 
   def test_first_verse
-    expected_verse = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
+    expected_verse = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall."
     assert_equal(expected_verse, verse)
   end
 
   def test_second_verse
-    expected_verse = "98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around 97 bottles of beer on the wall."
+    expected_verse = "98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around, 97 bottles of beer on the wall."
     assert_equal(expected_verse, verse(98))
   end
 
