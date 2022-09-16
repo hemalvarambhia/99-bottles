1977e0a963ff3b68fc4dc4edfae060c3d6caf4fe
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 17:48:58 +0100

    If new number do new thing, else do the old thing.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -1,12 +1,21 @@
 require 'minitest/autorun'
 
 class BottlesSongTest < Minitest::Test
-  def verse
-    "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
+  def verse(number = 99)
+    if number == 98
+      "98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around 97 bottles of beer on the wall."
+    else
+      "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
+    end
   end
 
   def test_first_verse
     expected_verse = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
     assert_equal(expected_verse, verse)
+  end
+
+  def test_second_verse
+    expected_verse = "98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around 97 bottles of beer on the wall."
+    assert_equal(expected_verse, verse(98))
   end
 end
