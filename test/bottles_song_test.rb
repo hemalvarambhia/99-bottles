c3f93365b82c74d7b857013c2b87445b408f1fdb
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-16 16:50:45 +0100

    Fake it till we make it. We can sing the first verse of the song.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -1,7 +1,12 @@
 require 'minitest/autorun'
 
 class BottlesSongTest < Minitest::Test
-  def test_failing_hookup
-    assert_equal(3, 1 + 1)
+  def verse
+    "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
+  end
+
+  def test_first_verse
+    expected_verse = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around 98 bottles of beer on the wall."
+    assert_equal(expected_verse, verse)
   end
 end
