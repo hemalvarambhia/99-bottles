efbbef8574e4356cc4bbb755d9a8a5d3735b0c8b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-17 23:01:17 +0100

    Inlined the method.

--- lib/bottle_number.rb
+++ lib/bottle_number.rb
@@ -4,17 +4,13 @@ class BottleNumber
   end
 
   def succ
-    successive_to(@number)
-  end
-
-  def successive_to(number)
-    case number
+    case @number
     when 0
       99
     when 1
       0
     else
-      number - 1
+      @number - 1
     end
   end
 end
