6e1b06013492f33b9671f0032d8d78bce24425e4
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-17 18:21:24 +0100

    Clarified some expected behaviour.

--- test/bottles_song_test.rb
+++ test/bottles_song_test.rb
@@ -6,27 +6,27 @@ class BottlesSongTest < Minitest::Test
   end
 
   def test_first_verse
-    expected_verse = "99 bottles of beer on the wall, 99 bottles of beer. Take one down and pass it around, 98 bottles of beer on the wall."
+    expected_verse = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall."
     assert_equal(expected_verse, verse(99))
   end
 
   def test_second_verse
-    expected_verse = "98 bottles of beer on the wall, 98 bottles of beer. Take one down and pass it around, 97 bottles of beer on the wall."
+    expected_verse = "98 bottles of beer on the wall, 98 bottles of beer.\nTake one down and pass it around, 97 bottles of beer on the wall."
     assert_equal(expected_verse, verse(98))
   end
 
   def test_verse_97
-    expected_verse = "2 bottles of beer on the wall, 2 bottles of beer. Take one down and pass it around, 1 bottle of beer on the wall."
+    expected_verse = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."
     assert_equal(expected_verse, verse(2))
   end
 
   def test_verse_98
-    expected_verse = "1 bottle of beer on the wall, 1 bottle of beer. Take it down and pass it around, no more bottles of beer on the wall."
+    expected_verse = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."
     assert_equal(expected_verse, verse(1))
   end
 
   def test_last_verse
-    expected_verse = "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    expected_verse = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
     assert_equal(expected_verse, verse(0))
   end
 end
