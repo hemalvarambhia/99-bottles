5e65c4cadb429fe839a3277235e45911bab7f01b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-18 21:28:06 +0100

    Extracted an intention-revealing local variable.

--- lib/bottle_song.rb
+++ lib/bottle_song.rb
@@ -2,6 +2,7 @@ require_relative './bottle_number'
 class BottleSong
   def verse(number)
     current_bottle_number = BottleNumber.new(number)
-    "#{current_bottle_number.to_s} #{current_bottle_number.container} of beer on the wall, #{current_bottle_number.to_s.downcase} #{current_bottle_number.container} of beer.\n#{current_bottle_number.action}, #{current_bottle_number.succ.to_s.downcase} #{current_bottle_number.succ.container} of beer on the wall."
+    next_bottle_number = current_bottle_number.succ
+    "#{current_bottle_number.to_s} #{current_bottle_number.container} of beer on the wall, #{current_bottle_number.to_s.downcase} #{current_bottle_number.container} of beer.\n#{current_bottle_number.action}, #{next_bottle_number.to_s.downcase} #{next_bottle_number.container} of beer on the wall."
   end
 end
