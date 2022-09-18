210bd159030d0205eef619e30173b1244da1207e
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-18 21:26:44 +0100

    Inlined the method as it does too little.

--- lib/bottle_song.rb
+++ lib/bottle_song.rb
@@ -2,12 +2,6 @@ require_relative './bottle_number'
 class BottleSong
   def verse(number)
     current_bottle_number = BottleNumber.new(number)
-    "#{current_bottle_number.to_s} #{current_bottle_number.container} of beer on the wall, #{current_bottle_number.to_s.downcase} #{current_bottle_number.container} of beer.\n#{action(current_bottle_number)}, #{current_bottle_number.succ.to_s.downcase} #{current_bottle_number.succ.container} of beer on the wall."
-  end
-
-  private
-
-  def action(number)
-    number.action
+    "#{current_bottle_number.to_s} #{current_bottle_number.container} of beer on the wall, #{current_bottle_number.to_s.downcase} #{current_bottle_number.container} of beer.\n#{current_bottle_number.action}, #{current_bottle_number.succ.to_s.downcase} #{current_bottle_number.succ.container} of beer on the wall."
   end
 end
