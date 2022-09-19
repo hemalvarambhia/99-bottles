fc1c0dbae440923997609c77c7235a155cfe8d15
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-19 22:26:40 +0100

    We can now sing 99 verses of the Bottles of Beer song.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -2,7 +2,10 @@ require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
-    if number == 2
+    if number == 1
+      "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
+        "Take it down and pass it around, No more bottles of beer on the wall.\n"
+    elsif number == 2
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
         "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
     else
@@ -33,5 +36,13 @@ class BottlesOfBeerSongTest < Minitest::Test
         "Take one down and pass it around, 1 bottle of beer on the wall.\n"
 
     assert_equal(expected_verse, verse(2))
+  end
+
+  def test_verse_99
+    expected_verse =
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+        "Take it down and pass it around, No more bottles of beer on the wall.\n"
+
+    assert_equal(expected_verse, verse(1))
   end
 end
