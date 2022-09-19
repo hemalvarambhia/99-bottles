8a0ea79507a53022a30e827dbf6477f799abf411
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-19 15:16:56 +0100

    Used the If New Thing, Do New Thing, Else Do The Old Thing.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -1,9 +1,14 @@
 require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
-  def verse
-    "99 bottle of beer on the wall, 99 bottle of beer.\n" +
-      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse(number = 99)
+    if number == 98
+      "98 bottle of beer on the wall, 98 bottle of beer.\n" +
+        "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+    else
+      "99 bottle of beer on the wall, 99 bottle of beer.\n" +
+        "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+    end
   end
 
   def test_verse_1
@@ -12,5 +17,13 @@ class BottlesOfBeerSongTest < Minitest::Test
       "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
     assert_equal(expected_verse, verse)
+  end
+
+  def test_verse_2
+    expected_verse =
+      "98 bottle of beer on the wall, 98 bottle of beer.\n" +
+        "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+
+    assert_equal(expected_verse, verse(98))
   end
 end
