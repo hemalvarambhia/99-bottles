f086b4e40d4a8bd581ed6e634c2e743f9ec263be
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-19 15:13:59 +0100

    Used fake it til you make it to get the first test passing.

--- test/bottles_of_bear_song_test.rb
+++ test/bottles_of_bear_song_test.rb
@@ -1,7 +1,16 @@
 require 'minitest/autorun'
 
 class BottlesOfBearSongTest < Minitest::Test
-  def hookup_test
-    assert_equal(4, 2 + 2)
+  def verse
+    "99 bottle of beer on the wall, 99 bottle of beer.\n" +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  end
+
+  def test_verse_1
+    expected_verse =
+      "99 bottle of beer on the wall, 99 bottle of beer.\n" +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+
+    assert_equal(expected_verse, verse)
   end
 end
