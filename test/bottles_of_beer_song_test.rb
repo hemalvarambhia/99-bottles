2faac8302f20ff085019a732b50609fe32b060f7
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-19 15:24:19 +0100

    Pushed a detail closer to the test, inside the test.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
-  def verse(number = 99)
+  def verse(number)
     "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
       "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
   end
@@ -11,7 +11,7 @@ class BottlesOfBeerSongTest < Minitest::Test
       "99 bottle of beer on the wall, 99 bottle of beer.\n" +
       "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
-    assert_equal(expected_verse, verse)
+    assert_equal(expected_verse, verse(99))
   end
 
   def test_verse_2
