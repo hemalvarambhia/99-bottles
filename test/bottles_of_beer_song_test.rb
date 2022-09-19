36cfa07a59a5768d646bca2cf7f8def315966336
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-19 15:27:22 +0100

    Corrected the expected value and the actual behaviour.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -2,13 +2,13 @@ require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
-    "#{number} bottle of beer on the wall, #{number} bottle of beer.\n" +
+    "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
   end
 
   def test_verse_1
     expected_verse =
-      "99 bottle of beer on the wall, 99 bottle of beer.\n" +
+      "99 bottles of beer on the wall, 99 bottles of beer.\n" +
       "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
     assert_equal(expected_verse, verse(99))
@@ -16,7 +16,7 @@ class BottlesOfBeerSongTest < Minitest::Test
 
   def test_verse_2
     expected_verse =
-      "98 bottle of beer on the wall, 98 bottle of beer.\n" +
+      "98 bottles of beer on the wall, 98 bottles of beer.\n" +
         "Take one down and pass it around, 97 bottles of beer on the wall.\n"
 
     assert_equal(expected_verse, verse(98))
