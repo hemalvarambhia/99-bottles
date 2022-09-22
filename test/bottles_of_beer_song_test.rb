881c573fed42d7d4a267843c84fbd06e08cd14bb
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-22 21:43:33 +0100

    Removed obsolete code.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -3,40 +3,7 @@ require_relative '../lib/bottle_verse'
 
 class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
-    "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
-      "#{action(number)}, #{quantity_and_bottles(number_successive_to(number))} of beer on the wall.\n"
     BottleVerse.new.verse(number)
-  end
-
-  def number_successive_to(number)
-    case number
-    when 0
-      99
-    else
-      number - 1
-    end
-  end
-
-  def quantity_and_bottles(number)
-    case number
-    when 0
-      "No more bottles"
-    when 1
-      "#{number} bottle"
-    else
-      "#{number} bottles"
-    end
-  end
-
-  def action(number)
-    case number
-    when 0
-      "Go to the store and by some more"
-    when 1
-      "Take it down and pass it around"
-    else
-      "Take one down and pass it around"
-    end
   end
 
   def test_verse_1
