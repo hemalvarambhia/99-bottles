a2e102a580babe97cedc1f9677c27cfb2603a435
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-23 14:03:20 +0100

    Inlined the method.

--- lib/bottles_of_beer_song.rb
+++ lib/bottles_of_beer_song.rb
@@ -2,16 +2,12 @@ require_relative './bottles_of_beer_verse'
 class BottlesOfBeerSong
   def verse(number)
     verse = BottlesOfBeerVerse.new(number)
-    next_verse = number_successive_to(verse)
+    next_verse = verse.number_successive_to
     "#{quantity_and_bottles(verse)} of beer on the wall, #{quantity_and_bottles(verse).downcase} of beer.\n" +
       "#{action(verse)}, #{quantity_and_bottles(next_verse)} of beer on the wall.\n"
   end
 
   private
-
-  def number_successive_to(bottle_verse)
-    bottle_verse.number_successive_to
-  end
 
   def quantity_and_bottles(bottle_verse)
     "#{bottle_verse.quantity} #{bottle_verse.container}"
