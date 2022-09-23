edf35b073d2a02ec5ba53a6d9f940f04465f1da0
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-23 14:02:11 +0100

    Removed an unused variable.

--- lib/bottles_of_beer_song.rb
+++ lib/bottles_of_beer_song.rb
@@ -4,7 +4,7 @@ class BottlesOfBeerSong
     verse = BottlesOfBeerVerse.new(number)
     next_verse = number_successive_to(verse)
     "#{quantity_and_bottles(verse)} of beer on the wall, #{quantity_and_bottles(verse).downcase} of beer.\n" +
-      "#{action(number, verse)}, #{quantity_and_bottles(next_verse)} of beer on the wall.\n"
+      "#{action(verse)}, #{quantity_and_bottles(next_verse)} of beer on the wall.\n"
   end
 
   private
@@ -17,7 +17,7 @@ class BottlesOfBeerSong
     "#{bottle_verse.quantity} #{bottle_verse.container}"
   end
 
-  def action(number, bottle_verse = nil)
+  def action(bottle_verse)
     bottle_verse.action
   end
 end
