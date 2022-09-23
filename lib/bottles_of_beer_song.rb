018f7968afed52d585b92818d9ff3dafdef062e9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-23 13:57:41 +0100

    Next verse no longer needs the number.

--- lib/bottles_of_beer_song.rb
+++ lib/bottles_of_beer_song.rb
@@ -2,14 +2,14 @@ require_relative './bottles_of_beer_verse'
 class BottlesOfBeerSong
   def verse(number)
     verse = BottlesOfBeerVerse.new(number)
-    next_verse = number_successive_to(number, verse)
+    next_verse = number_successive_to(verse)
     "#{quantity_and_bottles(number, verse)} of beer on the wall, #{quantity_and_bottles(number, verse).downcase} of beer.\n" +
       "#{action(number, verse)}, #{quantity_and_bottles(next_verse.to_i, next_verse)} of beer on the wall.\n"
   end
 
   private
 
-  def number_successive_to(number, bottle_verse = nil)
+  def number_successive_to(bottle_verse)
     bottle_verse.number_successive_to
   end
 
