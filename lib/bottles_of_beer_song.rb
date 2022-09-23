34cf51a30160c96973d7db09e9d8198a548ab4d9
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-09-23 14:00:10 +0100

    Removed the number.

--- lib/bottles_of_beer_song.rb
+++ lib/bottles_of_beer_song.rb
@@ -3,8 +3,8 @@ class BottlesOfBeerSong
   def verse(number)
     verse = BottlesOfBeerVerse.new(number)
     next_verse = number_successive_to(verse)
-    "#{quantity_and_bottles(number, verse)} of beer on the wall, #{quantity_and_bottles(number, verse).downcase} of beer.\n" +
-      "#{action(number, verse)}, #{quantity_and_bottles(next_verse.to_i, next_verse)} of beer on the wall.\n"
+    "#{quantity_and_bottles(verse)} of beer on the wall, #{quantity_and_bottles(verse).downcase} of beer.\n" +
+      "#{action(number, verse)}, #{quantity_and_bottles(next_verse)} of beer on the wall.\n"
   end
 
   private
@@ -13,7 +13,7 @@ class BottlesOfBeerSong
     bottle_verse.number_successive_to
   end
 
-  def quantity_and_bottles(number = nil, bottle_verse = nil)
+  def quantity_and_bottles(bottle_verse)
     "#{bottle_verse.quantity} #{bottle_verse.container}"
   end
 
