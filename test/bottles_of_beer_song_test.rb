1a1abd083baf54d22fda5b8e9e378cb507e246d1
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2022-10-07 12:24:26 -0400

    fake it til you make it

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -8,4 +8,9 @@ class BottlesOfBeerSongTest < Minitest::Test
 
     assert_equal(expected_verse, verse(99))
   end
+
+  def verse(num)
+    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  end
 end
