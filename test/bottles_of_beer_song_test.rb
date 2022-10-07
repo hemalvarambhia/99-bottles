127becef9c64c6f9b7ce30337d2560cd3e6300d5
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2022-10-07 13:05:40 -0400

    took out skip

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -15,7 +15,6 @@ class BottlesOfBeerSongTest < Minitest::Test
   end
 
   def test_verse_2
-    skip('Get previous test passing')
     expected_verse =
       "98 bottles of beer on the wall, 98 bottles of beer.\n" +
         "Take one down and pass it around, 97 bottles of beer on the wall.\n"
