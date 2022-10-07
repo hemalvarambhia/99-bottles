225729b3b92cf8c302bd4a0b50f127c3e879c657
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-07 17:33:10 +0100

    Added the next failing test.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -8,4 +8,12 @@ class BottlesOfBeerSongTest < Minitest::Test
 
     assert_equal(expected_verse, verse(99))
   end
+
+  def test_verse_2
+    expected_verse =
+      "98 bottles of beer on the wall, 98 bottles of beer.\n" +
+        "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+
+    assert_equal(expected_verse, verse(98))
+  end
 end
