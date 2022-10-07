aa549c43109ca014703bdb0c63a280760a0cbe5c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-07 17:46:52 +0100

    Undid Peter's change and skipped next test for new comers.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -8,17 +8,13 @@ class BottlesOfBeerSongTest < Minitest::Test
 
     assert_equal(expected_verse, verse(99))
   end
-  
-  def verse(num)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-      "Take one down and pass it around, 98 bottles of beer on the wall.\n"
 
   def test_verse_2
+    skip('Get previous test passing')
     expected_verse =
       "98 bottles of beer on the wall, 98 bottles of beer.\n" +
         "Take one down and pass it around, 97 bottles of beer on the wall.\n"
 
     assert_equal(expected_verse, verse(98))
-
   end
 end
