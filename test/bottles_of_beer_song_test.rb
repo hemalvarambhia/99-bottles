5fe4fc9a09f341150437133f59be825b3aa660fc
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-07 18:11:06 +0100

    Added the next test to get passing.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -17,4 +17,13 @@ class BottlesOfBeerSongTest < Minitest::Test
 
     assert_equal(expected_verse, verse(98))
   end
+
+  def test_verse_97
+    skip('Get previous test passing')
+    expected_verse =
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+
+    assert_equal(expected_verse, verse(2))
+  end
 end
