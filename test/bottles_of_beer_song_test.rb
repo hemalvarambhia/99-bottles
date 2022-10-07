1b9b3eef4b5dbd7e385721ae3e314bbdf1f51d72
Alexandra Borge <alexandra.borge@simplybusiness.com>
2022-10-07 12:24:37 -0400

    Fake it til you make it

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -1,6 +1,11 @@
 require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
+  def verse(num = 99)
+    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
+        "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  end
+
   def test_verse_1
     expected_verse =
       "99 bottles of beer on the wall, 99 bottles of beer.\n" +
