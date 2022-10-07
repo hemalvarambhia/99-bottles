46d8a94bb10fdef859706d5f253b8bbf4c585154
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2022-10-07 13:01:18 -0400

    #{num}

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -1,6 +1,11 @@
 require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
+
+  def verse(num)
+    "#{num} bottles of beer on the wall, #{num} bottles of beer.\n" +
+      "Take one down and pass it around, #{num - 1} bottles of beer on the wall.\n"
+  end
   def test_verse_1
     expected_verse =
       "99 bottles of beer on the wall, 99 bottles of beer.\n" +
