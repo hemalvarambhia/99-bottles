2383f1b4d6094cf5eaeee99c1b2df19fc3d5e837
Alexandra Borge <alexandra.borge@simplybusiness.com>
2022-10-13 14:20:23 -0400

    both verse methods were sharing the same pattern so I made the similar code more similar by interpolating a number into the verses.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -1,14 +1,14 @@
 require 'minitest/autorun'
 
 class BottlesOfBeerSongTest < Minitest::Test
-  def verse(num = 99)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" +
-        "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+  def verse(starting_number_of_bottles = 99)
+    "#{starting_number_of_bottles} bottles of beer on the wall, #{starting_number_of_bottles} bottles of beer.\n" +
+        "Take one down and pass it around, #{starting_number_of_bottles - 1} bottles of beer on the wall.\n"
   end
 
-  def verse_2(num = 98)
-    "98 bottles of beer on the wall, 98 bottles of beer.\n" +
-        "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+  def verse_2(starting_number_of_bottles = 98)
+    "#{starting_number_of_bottles} bottles of beer on the wall, #{starting_number_of_bottles} bottles of beer.\n" +
+        "Take one down and pass it around, #{starting_number_of_bottles - 1} bottles of beer on the wall.\n"
   end
 
   def test_verse_1
@@ -23,6 +23,8 @@ class BottlesOfBeerSongTest < Minitest::Test
   def test_verse_2
     # updating the verse made the first test fail because the number of bottles is wrong
     # to get this passing, I created a verse_2 method
+    # noticing that verse 1 and verse 2 use the same pattern,
+    # I used the strategy of making the similar code more similar by interpolating the "starting_number_of_bottles" into each verse
     expected_verse =
       "98 bottles of beer on the wall, 98 bottles of beer.\n" +
         "Take one down and pass it around, 97 bottles of beer on the wall.\n"
