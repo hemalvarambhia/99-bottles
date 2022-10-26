950760aca1cd7ca92579221b7b153cda85432ac5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-26 15:45:46 -0400

    Triangulation - remove hard coded data.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -3,8 +3,8 @@ require 'minitest/autorun'
 class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     if number == 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+        "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
         "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
