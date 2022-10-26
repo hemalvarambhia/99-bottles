4deeb664c5ba0f61e460a0dbee3873f2a17984b5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-26 15:41:02 -0400

    Triangulated.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -3,8 +3,8 @@ require 'minitest/autorun'
 class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     if number == 98
-      "98 bottles of beer on the wall, 98 bottles of beer.\n" +
-        "Take one down and pass it around, 97 bottles of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+        "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
     else
       "99 bottles of beer on the wall, 99 bottles of beer.\n" +
         "Take one down and pass it around, 98 bottles of beer on the wall.\n"
