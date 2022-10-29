9555e5d386e6929ba3c6acb549cf18941e1c4045
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-29 18:42:44 +0100

    Back to shameless green as per the book.

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -4,27 +4,17 @@ class BottlesOfBeerSongTest < Minitest::Test
   def verse(number)
     case number
     when 0
-      "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
         "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
-      "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number)} of beer.\n" +
-        "Take it down and pass it around, #{quantity_and_bottles(number - 1).downcase} of beer on the wall.\n"
+      "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number)} of beer.\n" +
-        "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     else
-      "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number)} of beer.\n" +
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
         "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    end
-  end
-
-  def quantity_and_bottles(number)
-    if number == 1
-      "1 bottle"
-    elsif number == 0
-      "No more bottles"
-    else
-      "#{number} bottles"
     end
   end
 
