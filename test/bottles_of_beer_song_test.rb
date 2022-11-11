46824763dc164a764983b40b4764c31c8f625e81
Alexandra Borge <alexandra.borge@simplybusiness.com>
2022-11-11 11:45:12 -0500

    extract numbers of bottles from second line of verses by using the quantity_and_bottles method

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -5,16 +5,16 @@ class BottlesOfBeerSongTest < Minitest::Test
     case number
     when 0
       "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
-        "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        "Go to the store and buy some more, #{quantity_and_bottles(99).downcase} of beer on the wall.\n"
     when 1
       "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
-        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+        "Take it down and pass it around, #{quantity_and_bottles(0).downcase} of beer on the wall.\n"
     when 2
       "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
-        "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+        "Take one down and pass it around, #{quantity_and_bottles(1).downcase} of beer on the wall.\n"
     else
       "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n" +
-        "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+        "Take one down and pass it around, #{quantity_and_bottles(number - 1).downcase} of beer on the wall.\n"
     end
   end
 
