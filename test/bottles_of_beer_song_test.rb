c391deee31410bc9c6e9510b8683540d9ccfe8b3
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2023-01-06 09:27:23 -0500

    clean up test

--- test/bottles_of_beer_song_test.rb
+++ test/bottles_of_beer_song_test.rb
@@ -6,54 +6,6 @@ class BottlesOfBeerSongTest < Minitest::Test
     GenericVerse.new.verse(number)
   end
 
-  def first_sentence(number)
-    "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n"
-  end
-
-  def second_sentence(number)
-    action(number) + ", #{left_over_bottles(number).downcase} of beer on the wall.\n"
-  end
-
-  def action(number)
-    if number == 0
-      "Go to the store and buy some more"
-    else
-      "Take #{pronoun(number)} down and pass it around"
-    end
-  end
-
-  def pronoun(number)
-    if number == 1
-      "it"
-    else
-      "one"
-    end
-  end
-
-  def left_over_bottles(number)
-    if number == 0
-      quantity_and_bottles(99)
-    else
-      quantity_and_bottles(number - 1)
-    end
-  end
-
-  def quantity_and_bottles(number)
-    if number == 0
-      "No more #{bottles(number)}"
-    else
-      "#{number} #{bottles(number)}"
-    end
-  end
-
-  def bottles(number)
-    if number == 1
-      "bottle"
-    else
-      "bottles"
-    end
-  end
-
   def test_verse_1
     expected_verse =
       "99 bottles of beer on the wall, 99 bottles of beer.\n" +
