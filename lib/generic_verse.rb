82dd7764a4c127a19ef414b001cea1c82b2c0359
Peter Walker <108812366+petertimwalker@users.noreply.github.com>
2023-01-06 09:46:15 -0500

    refactor state variable name

--- lib/generic_verse.rb
+++ lib/generic_verse.rb
@@ -1,27 +1,27 @@
 class GenericVerse
 
   def initialize(number)
-    @number = number
+    @bottle_number = number
   end
 
   def verse(number)
-    first_sentence(@number) + second_sentence(@number)
+    first_sentence(@bottle_number) + second_sentence(@bottle_number)
   end
 
   def first_sentence(number)
-    "#{quantity_and_bottles(number)} of beer on the wall, #{quantity_and_bottles(number).downcase} of beer.\n"
+    "#{quantity_and_bottles(@bottle_number)} of beer on the wall, #{quantity_and_bottles(@bottle_number).downcase} of beer.\n"
   end
 
   def second_sentence(number)
-    action(number) + ", #{left_over_bottles(number).downcase} of beer on the wall.\n"
+    action(@bottle_number) + ", #{left_over_bottles(@bottle_number).downcase} of beer on the wall.\n"
   end
 
   def action(number)
-    "Take #{pronoun(number)} down and pass it around"
+    "Take #{pronoun(@bottle_number)} down and pass it around"
   end
 
   def pronoun(number)
-    if number == 1
+    if @bottle_number == 1
       "it"
     else
       "one"
@@ -29,23 +29,23 @@ class GenericVerse
   end
 
   def left_over_bottles(number)
-    if number == 0
+    if @bottle_number == 0
       quantity_and_bottles(99)
     else
-      quantity_and_bottles(number - 1)
+      quantity_and_bottles(@bottle_number - 1)
     end
   end
 
   def quantity_and_bottles(number)
-    if number == 0
-      "No more #{bottles(number)}"
+    if @bottle_number == 0
+      "No more #{bottles(@bottle_number)}"
     else
-      "#{number} #{bottles(number)}"
+      "#{@bottle_number} #{bottles(@bottle_number)}"
     end
   end
 
   def bottles(number)
-    if number == 1
+    if @bottle_number == 1
       "bottle"
     else
       "bottles"
